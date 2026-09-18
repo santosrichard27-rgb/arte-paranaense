d72ca71d0184c85cb0a47369ad2cbe620a58b75c
santosrichard27-rgb <santos.richard27@escola.pr.gov.br>
2026-09-18 08:49:21 -0300

    atualizi o coigo

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+consst botoesCurtiu = documnt. qurySelectorall (".curtir");
+botoescurtir.forEach(function(botaocurtir) {
+    le curiu = false;
+    botocurtir.adddEventlinester("click", curtir);
+    function curtir ( ){
+const contador = botaocurtiu.querySelecor ("span");
+if (curtiu === false) {
+contador.textContent++;
+curtiu = true;}
+else{
+     contador.textContent --;
+     curtiu = fale;
+}
+}
 
+});
